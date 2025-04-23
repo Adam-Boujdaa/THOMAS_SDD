85b5f1cc04a37c9d305e1d59293ec6790e28fed0
AyoubBoudouhi <ayoub.boudouhi-mezroui@univ-tlse3.fr>
2025-04-23 19:26:50 +0200

    modif de voyageur.h

--- include/voyageur.h
+++ include/voyageur.h
@@ -1,30 +1,17 @@
-#ifndef __VOYAGEUR_H__
-#define __VOYAGEUR_H__
+#ifndef VOYAGEUR_H
+#define VOYAGEUR_H
 
-#include "trajet.h"
-#include "place.h"
+#define MAX_NOM 100
+#define MAX_TRAJETS 10
 
-typedef struct s_voyageur * Voyageur;
+typedef struct s_voyageur* Voyageur;
 
-// Création et destruction
-Voyageur voyageur_init(const char* nom, int id);
-void voyageur_effacer(Voyageur* voyageur);
 
-// Recherche d’un voyageur par identifiant
-Voyageur voyageur_rechercher_par_id(int id, Voyageur* liste, int taille);
+Voyageur creer_voyageur(const char* nom, int id);
+void reserver_trajet(Voyageur voyageur, int id_trajet);
+void modifier_trajet(Voyageur voyageur, int ancien_trajet, int nouveau_trajet);
+int trajet_existe(Voyageur voyageur, int id_trajet);
+void afficher_voyageur(Voyageur voyageur);
+void liberer_voyageur(Voyageur voyageur);
 
-// Réservation et modification de trajet
-int voyageur_reserver_trajet(Voyageur voyageur, Trajet trajet, Place* places, int nb_places);
-// Retourne 1 si réservation réussie, 0 sinon (places indisponibles, etc.)
-
-int voyageur_modifier_trajet(Voyageur voyageur, Trajet ancien, Trajet nouveau, Place* places, int nb_places);
-// Retourne 1 si modification réussie, 0 sinon
-
-// Navigation dans le menu voyageur (recherche par gare de départ/arrivée)
-void menu_voyageur(Voyageur* liste_voyageurs, int nb_voyageurs, Trajet* liste_trajets, int nb_trajets, Place* places, int nb_places);
-
-// Vérification de la cohérence réservation/places disponibles
-int trajet_places_disponibles(Trajet trajet, Place* places, int nb_places);
-// Retourne le nombre de places libres pour un trajet
-
-#endif //
+#endif
